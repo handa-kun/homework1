ce4da2630ef2d509dc81f27d1d0d15d58e6106ce
handa-kun <Whitey.A@yandex.ru>
2022-11-17 20:16:32 +0300

    btn-ham

--- main_scripts/main.js
+++ main_scripts/main.js
@@ -1,34 +1,7 @@
-/*$(function () {
-    $('.open-menu').click(function () {
-        $('.hbl').toggleClass('.shown-menu')
-    })
-}); */
-
-
-
-
-//validate_form 
-
-const re = new RegExp('/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/');
-
-function validateEmail(email) {
-    return re.test(email);
-};
-
-$('#s_email').click((e) => {
-    e.preventDefault();
-    const email = $('form#justEmail').serializeArray()[0].value;
-    alert(validateEmail(email));
+$(function () {
+    $('.open-menu').click(function (){
+        $('.hbl').toggleClass('showMenu')
+    });
 });
 
-/*$('button.btn').click((e) => {
-    e.preventDefault();
-    console.log($('form#form').serializeArray());
-    const login = $('form#form').serializeArray()[0].value;
-    console.log(validateLogin(login));
-    if (validateLogin(login)) {
-       $('p.result').html('All right!')
-    } else {
-        $('p.result').html('Try again.')
-    }
-});*/
+  
