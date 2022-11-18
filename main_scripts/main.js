8a89a39ab8a203fec16d59b12030f8f20840bce9
handa-kun <Whitey.A@yandex.ru>
2022-11-18 19:35:15 +0300

    added validate; hw#3 is done

--- main_scripts/main.js
+++ main_scripts/main.js
@@ -1,14 +1,45 @@
+//hamburger-btn
 $(function () {
     $('.open-menu').click(function (){
         $('.hbl').toggleClass('showMenu')
     });
 });
 
+//validate 
+const re = new RegExp('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,4}$');
+
+function validateEmail(email) {
+    return re.test(email);
+}
 
 $('#s_email').click((e) => {
-    e.preventDefault();
+    console.log(navigator.userAgent);
     console.log($('form#justEmail').serializeArray());
     const email = $('form#justEmail').serializeArray()[0].value;
-    alert(email);
-    console.log(navigator.userAgent);
+    console.log(validateEmail(email));
+    if (validateEmail(email)) {
+       alert(email);
+    } else {
+        alert('Try again!');
+    }
+    e.preventDefault();
+    e.target.reset();
 });
+
+
+$('#full-form').click((e) => {
+    console.log(navigator.userAgent);
+    console.log($('form#fForm').serializeArray());
+    const login = $('form#fForm').serializeArray()[0].value;
+    const email = $('form#fForm').serializeArray()[1].value;
+    const text = $('form#fForm').serializeArray()[2].value;
+    console.log(validateEmail(email));
+    if (validateEmail(email)) {
+        alert(`${login}\n${email}\n${text}`);
+    } else {
+        alert('Try again!');
+    }
+    e.preventDefault();
+    e.target.reset();
+});
+
